724839410fe336694cf2689ef0738e762b95df7f
JT <justin.tullgren@gmail.com>
2016-03-06 13:40:28 -0500

    Cool i can get the value. But I am map, mapping... looks gross

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 const _ = require('ramda');
+const compose = _.compose;
+const map = _.map;
 const $ = require('jquery');
 const fantasy = require('ramda-fantasy');
 const IO = fantasy.IO;
-
 const kefir = require('kefir');
 
+// Helpers
 const log = x => { console.log(x); return x; }; // eslint-disable-line
 
 // listen :: String -> DOM -> Stream
@@ -12,13 +14,18 @@ const listen = _.curry((event, target) => {
 	return kefir.fromEvents(target, event);
 });
 
+// keyUpStream :: String -> Stream
 const keypUpStream = listen('keyup');
 
+// getDom :: String -> IO
 const getDom = node => IO(() => document.getElementById(node)); // eslint-disable-line
+
+// eventValue :: DomEvent -> Stream String
+const eventValue = compose(map(_.prop('value')), map(_.prop('target')));
 
 // IMPURE
 
 $(() => {
-	getDom('search').map(keypUpStream).map(log).runIO().onValue(log);
+	getDom('search').map(keypUpStream).map(eventValue).runIO().onValue(log);
 });
 
